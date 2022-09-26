a1635852a7528991e4c30bf35df01ddda6a2baf2
Luisa Gavlak <80012121+luhgavlak@users.noreply.github.com>
2022-09-26 17:03:07 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -111,3 +111,99 @@ class State {
     }
 }
 
+class Game {  
+    constructor(state) {  
+      this.state = state;  
+      this.tickId = null;  
+      this.tick = this.tick.bind(this);  
+      this.render();  
+      this.handleClickBox = this.handleClickBox.bind(this);  
+    }  
+    
+    static ready() {  
+      return new Game(State.ready());  
+    }  
+    
+    tick() {  
+      this.setState({ time: this.state.time + 1 });  
+    }  
+    
+    setState(newState) {  
+      this.state = { ...this.state, ...newState };  
+      this.render();  
+    }  
+    
+    handleClickBox(box) {  
+      return function() {  
+        const nextdoorBoxes = box.getNextdoorBoxes();  
+        const blankBox = nextdoorBoxes.find(  
+          nextdoorBox => this.state.grid[nextdoorBox.y][nextdoorBox.x] === 0  
+        );  
+        if (blankBox) {  
+          const newGrid = [...this.state.grid];  
+          swapBoxes(newGrid, box, blankBox);  
+          if (isSolved(newGrid)) {  
+            clearInterval(this.tickId);  
+            this.setState({  
+              status: "won",  
+              grid: newGrid,  
+              move: this.state.move + 1  
+            });  
+          } else {  
+            this.setState({  
+              grid: newGrid,  
+              move: this.state.move + 1  
+            });  
+          }  
+        }  
+      }.bind(this);  
+    }  
+    
+    render() {  
+      const { grid, move, time, status } = this.state;  
+    
+      // Render grid  
+      const newGrid = document.createElement("div");  
+      newGrid.className = "grid";  
+      for (let i = 0; i < 4; i++) {  
+        for (let j = 0; j < 4; j++) {  
+          const button = document.createElement("button");  
+    
+          if (status === "playing") {  
+            button.addEventListener("click", this.handleClickBox(new Box(j, i)));  
+          }  
+    
+          button.textContent = grid[i][j] === 0 ? "" : grid[i][j].toString();  
+          newGrid.appendChild(button);  
+        }  
+      }  
+      document.querySelector(".grid").replaceWith(newGrid);  
+    
+      // Render button  
+      const newButton = document.createElement("button");  
+      if (status === "ready") newButton.textContent = "Play";  
+      if (status === "playing") newButton.textContent = "Reset";  
+      if (status === "won") newButton.textContent = "Play";  
+      newButton.addEventListener("click", () => {  
+        clearInterval(this.tickId);  
+        this.tickId = setInterval(this.tick, 1000);  
+        this.setState(State.start());  
+      });  
+      document.querySelector(".footer button").replaceWith(newButton);  
+    
+      // Render move  
+      document.getElementById("move").textContent = `Move: ${move}`;  
+    
+      // Render time  
+      document.getElementById("time").textContent = `Time: ${time}`;  
+    
+      // Render message  
+      if (status === "won") {  
+        document.querySelector(".message").textContent = "You win!";  
+      } else {  
+        document.querySelector(".message").textContent = "";  
+      }  
+    }  
+  }  
+    
+  const GAME = Game.ready(); 
